b7a49e1bc0d8ba297bc92acf440aba67d7c03f31
AKosmachyov <a.kosmachyov@gmail.com>
2018-12-27 12:46:23 +0300

    Update www folder

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var camera, scene, renderer;
+var mesh;
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -27,17 +30,44 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-      
-        const btn = document.getElementById('btn');
-        btn.addEventListener('click', () => {
-          cordova.plugins.arkit.coolMethod();
-        });
+        cordova.plugins.arkit.coolMethod('', console.log, console.error);
+        initThreeJs();
+        animate();
     }
 };
 
 app.initialize();
+
+function initThreeJs() {
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+  camera.position.z = 400;
+	scene = new THREE.Scene();
+	var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
+	var material = new THREE.MeshBasicMaterial();
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  const container = document.getElementById('modelContainer');
+	container.appendChild( renderer.domElement );
+  window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function onDocumentMouseMove( event ) {
+  mouseX = ( event.clientX - windowHalfX );
+  mouseY = ( event.clientY - windowHalfY );
+}
+
+function animate() {
+  requestAnimationFrame( animate );
+  mesh.rotation.x += 0.005;
+  mesh.rotation.y += 0.01;
+  renderer.render( scene, camera );
+}
