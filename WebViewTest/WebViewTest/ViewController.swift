6a245381ec55d12753023784f4831600290a1945
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-01-11 13:35:02 +0300

    Initial commit

---
+++ WebViewTest/WebViewTest/ViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  WebViewTest
+//
+//  Created by Eugene Bokhan on 11/01/2019.
+//  Copyright © 2019 Eugene Bokhan. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    let getUrlAtDocumentStartScript = "GetUrlAtDocumentStart"
+    let getUrlAtDocumentEndScript = "GetUrlAtDocumentEnd"
+    
+    var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        addWebView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        webView.load(URLRequest(url: URL(string: "http://apple.com")!))
+    }
+    
+    func addWebView() {
+        let configuration = WKWebViewConfiguration()
+        configuration.addScript(script: WKUserScript.getUrlScript(scriptName: getUrlAtDocumentStartScript),
+                                scriptHandlerName:getUrlAtDocumentStartScript,
+                                scriptMessageHandler: self,
+                                injectionTime: .atDocumentStart)
+        configuration.addScript(script: WKUserScript.getUrlScript(scriptName: getUrlAtDocumentEndScript),
+                                scriptHandlerName:getUrlAtDocumentEndScript,
+                                scriptMessageHandler: self,
+                                injectionTime: .atDocumentEnd)
+        
+        let frame = CGRect(origin: .zero,
+                           size: CGSize(width: view.frame.width,
+                                        height: view.frame.height - 90))
+        
+        webView = WKWebView(frame: frame,
+                            configuration: configuration)
+        webView.navigationDelegate = self
+
+        webView.layer.masksToBounds = true
+        webView.layer.cornerRadius = 10
+        view.addSubview(webView)
+        
+    }
+
+
+}
+
+extension ViewController: WKNavigationDelegate {
+    
+}
+
+
+extension ViewController: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController,
+                               didReceive message: WKScriptMessage) {
+        switch message.name {
+            
+        case getUrlAtDocumentStartScript:
+            print("start: \(message.body)")
+            
+        case getUrlAtDocumentEndScript:
+            print("end: \(message.body)")
+            
+        default:
+            break;
+        }
+    }
+}
+
+extension WKUserScript {
+    class func getUrlScript(scriptName: String) -> String {
+        return "webkit.messageHandlers.\(scriptName).postMessage(document.URL)"
+    }
+}
+
+extension WKWebViewConfiguration {
+    func addScript(script: String,
+                   scriptHandlerName:String,
+                   scriptMessageHandler: WKScriptMessageHandler,
+                   injectionTime:WKUserScriptInjectionTime) {
+        let userScript = WKUserScript(source: script,
+                                      injectionTime: injectionTime,
+                                      forMainFrameOnly: false)
+        userContentController.addUserScript(userScript)
+        userContentController.add(scriptMessageHandler,
+                                  name: scriptHandlerName)
+    }
+}
+
+
--- src/ios/ArKitPlugin.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-@objc(HWPArKitPlugin) class ArKitPlugin : CDVPlugin {
-    @objc func coolMethod(_ command: CDVInvokedUrlCommand) {
-
-        let result = CDVPluginResult(status: CDVCommandStatus_OK)
-        commandDelegate!.send(result, callbackId: command.callbackId)
-    }
-}
